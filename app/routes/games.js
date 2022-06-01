73ebafc73365552c1cc63cde4625abdd8ecd7b67
NadymVanSchaik <34303676+NadymVanSchaik@users.noreply.github.com>
2022-06-01 15:02:45 +0200

    Created routes. Start on Game model

---
+++ app/routes/games.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const got = require('got');
+const { pipeline } = require('stream');
+
+// router.get('/', function(req, res) {
+//   const dataStream = got.stream({
+//       url: 'https://api.steampowered.com/ISteamApps/GetAppList/v2/',
+//     //   qs: {
+//     //     api_key: '123456',
+//     //     query: 'World of Warcraft: Legion'
+//     //   }
+//   });
+//   pipeline(dataStream, res, (err) => {
+//       if (err) {
+//           console.log(err);
+//           res.sendStatus(500);
+//       }
+//   });
+// });
+
+router.get('/all', (req, res) => {
+    res.send('get all games');
+});
+
+router.get('/:id', (req, res, next) => {
+    console.log(req.params.id); 
+    res.send('Get by id');
+    // etc ...
+});
+
+router.get('/popular', (req, res) => {
+    res.send('Get popular games');
+});
+
+router.get('/new', (req, res) => {
+    res.send('Get new games');
+});
+  
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const config = {
     name: 'game-service',
@@ -10,19 +12,23 @@ const config = {
 };
 
 const app = express();
+app.listen(config.port, () => {console.log("Server started and listing on ${config.port}")});
 const logger = log({ console: true, file: false, label: config.name });
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello world');
-});
+//Import
+const gamesRoute = require('./routes/games');
+app.use('/games', gamesRoute);
 
-app.listen(config.port, config.host, (e)=> {
-    if(e) {
-        throw new Error('Internal Server Error');
-    }
-    logger.info(`${config.name} running on ${config.host}:${config.port}`);
-});
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('we are on home');
+})
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to DB'))
